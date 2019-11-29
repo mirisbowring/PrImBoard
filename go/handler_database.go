6bc90efea10cf134f682d9d148d064d4fb013295
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-11-29 13:23:09 -0500

    Implemented DatabaseHandler

--- go/handler_database.go
+++ go/handler_database.go
@@ -4,23 +4,17 @@ import (
 	"context"
 	"time"
 	"log"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var (
-	// global db connection var
-	client *mongo.Client
-	dbClient *mongo.Database
-	DBName = "primboard"
-)
+// database namespace
+var DBName = "primboard"
 
-type Model interface {
-	GetCollection(db *mongo.Database) *mongo.Collection	
-}
-
-func Connect(){
+/*
+ * Initializes a mongodb connection
+ */
+func (a *App) Connect(){
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
@@ -28,36 +22,14 @@ func Connect(){
 	if err != nil {
 		log.Fatal(err)
 	}
-	dbClient = client.Database(DBName)
-}
-
-func FindOne(model Model, filter *bson.M) error {
-	col := model.GetCollection(dbClient)
-	ctx := dbContext(5)
-
-	err := col.FindOne(ctx, filter).Decode(&model)
-	return err
-}
-
-func InsertOne(model Model) (*mongo.InsertOneResult, error) {
-	col := model.GetCollection(dbClient)
-	ctx := dbContext(5)
-
-	result, err := col.InsertOne(ctx, model)
-	return result, err
-}
-
-func Save(model Model) (*mongo.InsertOneResult, error) {
-	col := model.GetCollection(dbClient)
-	ctx := dbContext(5)
-
-	result, err := col.InsertOne(ctx, model)
-	return result, err
+	a.DB = client.Database(DBName)
 }
 
 // helpers
-func dbContext(i time.Duration) context.Context {
-	ctx, cancel := context.WithTimeout(context.Background(), i*time.Second)
-	defer cancel()
+/*
+ * Creates a context of specified duration
+ */
+func DBContext(i time.Duration) context.Context {
+	ctx, _ := context.WithTimeout(context.Background(), i*time.Second)
 	return ctx
 }
