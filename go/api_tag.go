022ebeb4f76898a2e10ed841ba886f5ac0ac42c1
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-11-29 15:08:00 -0500

    implemented tag

--- go/api_tag.go
+++ go/api_tag.go
@@ -9,25 +9,109 @@
 package swagger
 
 import (
+	"encoding/json"
 	"net/http"
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+/*
+ * Handles the webrequest for Tag creation
+ */
 func (a *App) AddTag(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	var t Tag
+	// decode request into model
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&t); err != nil {
+		// an decode error occured
+		respondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+	defer r.Body.Close()
+	// name is mandatory
+	if t.Name == "" {
+		respondWithError(w, http.StatusBadRequest, "Tagname cannot be empty")
+		return
+	}
+	// try to insert model into db
+	result, err := t.AddTag(a.DB)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	// creation successful
+	respondWithJSON(w, http.StatusCreated, result)
 }
 
+/*
+ * Handles the webrequest for Tag deletion
+ */
 func (a *App) DeleteTagById(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	// parse request
+	vars := mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(vars["_id"])
+ 	// create model by passed id
+	t := Tag{Id: id}
+	// try to delete model
+	result, err := t.DeleteTag(a.DB)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	// deletion successful
+	respondWithJSON(w, http.StatusOK, result)
 }
 
+/*
+ * Handles the webrequest for receiving Tag model by id
+ */
 func (a *App) GetTagById(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+	// parse request
+	vars := mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(vars["_id"])
+	// create model by passed id
+	t := Tag{Id: id}
+	// try to select user
+	if err := t.GetTag(a.DB); err != nil {
+		switch err {
+			case mongo.ErrNoDocuments:
+				// model not found
+				respondWithError(w, http.StatusNotFound, "Tag not found")
+			default:
+				// another error occured
+				respondWithError(w, http.StatusInternalServerError, err.Error())
+		}
+		return
+	}
+	// could select user from mongo
+	respondWithJSON(w, http.StatusOK, t)
 }
 
-func (a *App) UpdateTagBayId(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
+/*
+ * Handles the webrequest for updating the Tag with the passed request body
+ */
+func (a *App) UpdateTagById(w http.ResponseWriter, r *http.Request) {
+	// parse request
+	vars := mux.Vars(r)
+	id, _ := primitive.ObjectIDFromHex(vars["_id"])
+	// store new model in tmp object
+	var ut Tag
+	decoder := json.NewDecoder(r.Body)
+	if err :=decoder.Decode(&ut); err != nil {
+		// error occured during encoding
+		respondWithError(w, http.StatusBadRequest, "Invalid Request payload")
+		return
+	}
+	defer r.Body.Close()
+	// trying to update model with requested body
+	t := Tag{Id: id}
+	result, err := t.UpdateTag(a.DB, ut)
+	if err != nil {
+		// Error occured during update
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	// Update successful
+	respondWithJSON(w, http.StatusOK, result)
 }
--- go/model_tag.go
+++ go/model_tag.go
@@ -8,9 +8,56 @@
  */
 package swagger
 
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
 type Tag struct {
+	Id primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Name string `json:"name,omitempty" bson:"name,omitempty"`
+}
 
-	Id int32 `json:"_id,omitempty"`
+// name of the mongo collection
+var tColName = "tag"
 
-	Name string `json:"name,omitempty"`
+/*
+ * Creates the model in the mongodb
+ */
+func (t *Tag) AddTag(db *mongo.Database) (*mongo.InsertOneResult, error) {
+	col, ctx := GetColCtx(tColName, db, 30)
+	result, err := col.InsertOne(ctx, t)
+	return result, err
+}
+
+/*
+ * Deletes the model from the mongodb
+ */
+ func (t *Tag) DeleteTag(db *mongo.Database) (*mongo.DeleteResult, error) {
+	col, ctx := GetColCtx(tColName, db, 30)
+	filter := bson.M{"_id": t.Id}
+	result, err := col.DeleteOne(ctx, filter)
+	return result, err
+}
+
+/*
+ * Returns the specified entry from the mongodb
+ */
+func (t *Tag) GetTag(db *mongo.Database) error {
+	col, ctx := GetColCtx(tColName, db, 30)
+	filter := bson.M{"_id": t.Id}
+	err := col.FindOne(ctx, filter).Decode(&t)
+	return err
+}
+
+/*
+ * Updates the record with the passed one
+ */
+func (t *Tag) UpdateTag(db *mongo.Database, ut Tag) (*mongo.UpdateResult, error) {
+	col, ctx := GetColCtx(tColName, db, 30)
+	filter := bson.M{"_id": t.Id}
+	update := bson.M{"$set": ut}
+	result, err := col.UpdateOne(ctx, filter, update)
+	return result, err
 }
