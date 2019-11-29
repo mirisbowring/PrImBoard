28173326cefde5c88d7ff22459968b924dc5aa67
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2019-11-29 15:04:30 -0500

    do not omitempty to prevent 0 collision

--- go/model_event.go
+++ go/model_event.go
@@ -15,7 +15,7 @@ import (
 )
 
 type Event struct {
-	Id primitive.ObjectID `json:"_id" bson:"_id"`
+	Id primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Title string `json:"title,omitempty" bson:"title,omitempty"`
 	Description string `json:"description,omitempty" bson:"description,omitempty"`
 	Comments []*Comment `json:"comments,omitempty" bson:"comments,omitempty"`
