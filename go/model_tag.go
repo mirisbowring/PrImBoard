9396e3d4d3b0ea29021ef2f55e04e0104d478f97
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2020-03-24 16:07:22 +0100

    added function to iteratively add tags

--- go/model_tag.go
+++ go/model_tag.go
@@ -1,6 +1,9 @@
 package primboard
 
 import (
+	"log"
+	"strings"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -22,6 +25,34 @@ func (t *Tag) AddTag(db *mongo.Database) (*mongo.InsertOneResult, error) {
 	result, err := col.InsertOne(ctx, t)
 	CloseContext()
 	return result, err
+}
+
+// addTags iterates over an array of tags and adds new one to the db
+func addTags(db *mongo.Database, tags []Tag) error {
+	for i := range tags {
+		if !tags[i].ID.IsZero() {
+			if err := tags[i].GetTag(db); err == nil {
+				// tag is valid and in db
+				continue
+			}
+			// tag is invalid and needs to be stored in the db
+			tags[i].ID = primitive.NilObjectID
+		}
+		tags[i].Name = strings.ToLower(tags[i].Name)
+		tags[i].Name = strings.TrimSpace(tags[i].Name)
+		// check if tag exists already
+		if err := tags[i].GetTagByName(db); err != nil {
+			switch err {
+			case mongo.ErrNoDocuments:
+				res, _ := tags[i].AddTag(db)
+				tags[i].ID = res.InsertedID.(primitive.ObjectID)
+				log.Printf("Created new tag <%s>", tags[i].Name)
+			default:
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 // DeleteTag deletes the model from the mongodb
