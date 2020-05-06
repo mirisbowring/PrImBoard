fd333a4738644a5521765be7baa63737e784d950
Arthur Ferdinand Lindner <arthur.lindner@outlook.de>
2020-05-06 10:58:08 +0200

    implemented verification process to usergroup

--- go/model_user_group.go
+++ go/model_user_group.go
@@ -1,18 +1,28 @@
 package primboard
 
 import (
+	"errors"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // UserGroup holts the users and the information about the group
 type UserGroup struct {
-	ID                primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Title             string             `json:"title,omitempty" bson:"title,omitempty"`
-	Creator           string             `json:"creator,omitempty" bson:"creator,omitempty"`
-	TimestampCreation int64              `json:"timestampCreation,omitempty" bson:"timestampCreation,omitempty"`
-	Users             []string           `json:"users,omitempty" bson:"users,omitempty"`
+	ID      primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Title   string             `json:"title,omitempty" bson:"title,omitempty"`
+	Creator string             `json:"creator,omitempty" bson:"creator,omitempty"`
+	Users   []string           `json:"users,omitempty" bson:"users,omitempty"`
+}
+
+//UserGroupProject is a bson representation of a user group
+var UserGroupProject = bson.M{
+	"_id":     1,
+	"title":   1,
+	"creator": 1,
+	"users":   1,
 }
 
 // name of the mongo collection
@@ -21,6 +31,10 @@ var ugColName = "usergroup"
 // AddUserGroup creates the model in the mongodb
 func (ug *UserGroup) AddUserGroup(db *mongo.Database) (*mongo.InsertOneResult, error) {
 	col, ctx := GetColCtx(ugColName, db, 30)
+	// verify that the fields are valid
+	if err := ug.Verify(db); err != nil {
+		return nil, err
+	}
 	result, err := col.InsertOne(ctx, ug)
 	CloseContext()
 	return result, err
@@ -44,12 +58,69 @@ func (ug *UserGroup) GetUserGroup(db *mongo.Database) error {
 	return err
 }
 
+// Save writes changes, made to the instance itself, to the database and
+// overrides the instance with the return value from the database
+func (ug *UserGroup) Save(db *mongo.Database) error {
+	col, ctx := GetColCtx(mediaColName, db, 30)
+	// verify that the fields are valid
+	if err := ug.Verify(db); err != nil {
+		return err
+	}
+	filter := bson.M{"_id": ug.ID}
+	update := bson.M{"$set": ug}
+	// options to return the update document
+	after := options.After
+	upsert := true
+	options := options.FindOneAndUpdateOptions{
+		ReturnDocument: &after,
+		Upsert:         &upsert,
+	}
+	// Execute query
+	err := col.FindOneAndUpdate(ctx, filter, update, &options).Decode(&ug)
+	CloseContext()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // UpdateUserGroup updates the record with the passed one
 func (ug *UserGroup) UpdateUserGroup(db *mongo.Database, uug UserGroup) (*mongo.UpdateResult, error) {
 	col, ctx := GetColCtx(ugColName, db, 30)
+	// verify that the fields are valid
+	if err := ug.Verify(db); err != nil {
+		return nil, err
+	}
+	// verify that the IDs are matching
+	if ug.ID != uug.ID {
+		return nil, errors.New("usergroup ids do not match")
+	}
 	filter := bson.M{"_id": ug.ID}
 	update := bson.M{"$set": uug}
 	result, err := col.UpdateOne(ctx, filter, update)
 	CloseContext()
 	return result, err
+}
+
+// Verify tries to verify the usergroup object
+func (ug *UserGroup) Verify(db *mongo.Database) error {
+	// verify title
+	if ug.Title == "" {
+		return errors.New("the title is not set")
+	}
+	// verify creator
+	u := User{Username: ug.Creator}
+	if !u.Exists(db) {
+		return errors.New("the creator does not exist")
+	}
+	// verify that creator is in group
+	if _, found := Find(ug.Users, ug.Creator); !found {
+		ug.Users = append(ug.Users, ug.Creator)
+	}
+	// verify that there are no duplicates
+	if len(ug.Users) > 1 {
+		ug.Users = UniqueStrings(ug.Users)
+	}
+
+	return nil
 }
